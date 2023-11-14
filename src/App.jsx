c296467cf9064fdf660feb74dfbb1d0dc007a887
Hasan <thehasandev@gmail.com>
2023-11-14 11:03:44 +0600

    font-family-add

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import React from 'react'
 function App() {
   return (
     <div>
-      <h1>Tailwind setup</h1>
+      <h1 className='text-5xl font-pop'>Tailwind setup</h1>
+      <h1 className='text-5xl font-roboto'>Tailwind setup</h1>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'pop': ['Poppins'],
+        'roboto': ['Roboto'],
+    
+      }
+    },
   },
   plugins: [],
 }
