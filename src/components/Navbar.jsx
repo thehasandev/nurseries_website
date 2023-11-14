601bcdbbbd57ba95d3f98a8f61a84a740de9bd21
Hasan <thehasandev@gmail.com>
2023-11-14 11:49:53 +0600

    react-icons

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Image from "../components/Image"
 import Logo from "../assets/logo.png"
 import Container from '../components/Container'
+import {RiArrowDropDownLine} from "react-icons/ri"
 function Navbar() {
   return (
     <Container>
@@ -12,7 +13,7 @@ function Navbar() {
           <div className='w-10/12 '>
             <ul className='flex items-center justify-end gap-x-5'>
               <li className='font-pop font-normal text-sm text-secondary hover:text-primary duration-300 cursor-pointer'>Home</li>
-              <li className='font-pop font-normal text-sm text-secondary hover:text-primary duration-300 cursor-pointer'>Our Products</li>
+              <li className='font-pop font-normal text-sm text-secondary hover:text-primary duration-300 cursor-pointer flex items-center'>Our Products<RiArrowDropDownLine size={25}/></li>
               <li className='font-pop font-normal text-sm text-secondary hover:text-primary duration-300 cursor-pointer'>Furniture Gallery</li>
               <li className='font-pop font-normal text-sm text-secondary hover:text-primary duration-300 cursor-pointer'>Tree Gallery</li>
               <li className='font-pop font-normal text-sm text-secondary hover:text-primary duration-300 cursor-pointer'>Care & Maintenance</li>
