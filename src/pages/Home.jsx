f941d622b9c03d5128e28fd355f95568ffa921a4
Hasan <thehasandev@gmail.com>
2023-11-15 13:31:02 +0600

    navbar-banner-responsibe

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -27,17 +27,17 @@ import GallarySix from "../assets/g6.jpg"
 function Home() {
   
   const settings = {
-    autoplay: true,
+    // autoplay: true,
     dots: true,
     infinite: true,
-    speed: 2500,
-    autoplaySpeed: 3000,
+    speed: 1000,
+    autoplaySpeed: 1000,
     slidesToShow: 1,
     slidesToScroll: 1,
     arrows:false,
     appendDots: dots => (
         <div>
-          <ul className='flex gap-x-4 absolute bottom-6 left-1/2 -translate-x-1/2'> {dots} </ul>
+          <ul className='flex gap-x-4 absolute bottom-[0] md:bottom-6 left-1/2 -translate-x-1/2'> {dots} </ul>
         </div>
       ),
       customPaging: i => (
@@ -62,16 +62,15 @@ function Home() {
 
   return (
     <>
-    <section>
-
+    <section className=''>
         <Slider {...settings}>
-          <div className=' bg-[#D6F7F2] h-[580px] w-full py-[100px]'>
+          <div className=' bg-[#D6F7F2] h-[300px] md:h-[580px] w-full py-[40px] md:py-[100px] px-2 xl:px-0'>
             <Container>
               <Flex className="justify-between pt-5">
                 <div className='w-4/12'>
-                  <p className='font-roboto font-normal text-lg text-secondary'>Get up to 50% off Today Only!</p>
-                  <h1 className='font-roboto font-bold my-5 text-[70px] leading-[75px] text-secondary'>Wooden Chair Collection</h1>
-                  <button className='font-pop font-normal text-base  text-secondary px-12 hover:bg-secondary duration-500 hover:text-white py-4 rounded-full border-solid border border-secondary'>Shop Now</button>
+                  <p className='font-roboto font-normal text-sm md:text-lg text-secondary'>Get up to 50% off Today Only!</p>
+                  <h1 className='font-roboto font-bold my-4 md:my-5 w-[200px] md:w-auto text-[28px] md:text-[70px] leading-9 md:leading-[75px] text-secondary'>Wooden Chair Collection</h1>
+                  <button className='font-pop text-sm font-normal md:text-base px-4 py-2 text-secondary md:px-12 hover:bg-secondary duration-500 hover:text-white md:py-4 rounded-full border-solid border border-secondary'>Shop Now</button>
                 </div>
                 <div className='w-6/12'>
                   <Image className="w-[500px]" src={One}/>
@@ -80,13 +79,13 @@ function Home() {
             </Container>
           </div>
 
-          <div className=' bg-[#D6F7F2] h-[580px] w-full py-[100px]'>
+          <div className=' bg-[#D6F7F2] h-[300px] md:h-[580px] w-full py-[40px] md:py-[100px] px-2 xl:px-0'>
             <Container>
               <Flex className="justify-between pt-5">
                 <div className='w-4/12'>
-                  <p className='font-roboto font-normal text-lg text-secondary'>Get up to 50% off Today Only!</p>
-                  <h1 className='font-roboto font-bold my-5 text-[70px] leading-[75px] text-secondary'>Wooden Chair Collection</h1>
-                  <button className='font-pop font-normal text-base  text-secondary px-12 hover:bg-secondary duration-500 hover:text-white py-4 rounded-full border-solid border border-secondary'>Shop Now</button>
+                  <p className='font-roboto font-normal text-sm md:text-lg text-secondary'>Get up to 50% off Today Only!</p>
+                  <h1 className='font-roboto font-bold my-4 md:my-5 w-[200px] md:w-auto text-[28px] md:text-[70px] leading-9 md:leading-[75px] text-secondary'>Wooden Chair Collection</h1>
+                  <button className='font-pop text-sm font-normal md:text-base px-4 py-2 text-secondary md:px-12 hover:bg-secondary duration-500 hover:text-white md:py-4 rounded-full border-solid border border-secondary'>Shop Now</button>
                 </div>
                 <div className='w-6/12'>
                   <Image className="w-[500px]" src={One}/>
@@ -95,13 +94,13 @@ function Home() {
             </Container>
           </div>
 
-          <div className=' bg-[#D6F7F2] h-[580px] w-full py-[100px]'>
+          <div className=' bg-[#D6F7F2] h-[300px] md:h-[580px] w-full py-[40px] md:py-[100px] px-2 xl:px-0'>
             <Container>
               <Flex className="justify-between pt-5">
                 <div className='w-4/12'>
-                  <p className='font-roboto font-normal text-lg text-secondary'>Get up to 50% off Today Only!</p>
-                  <h1 className='font-roboto font-bold my-5 text-[70px] leading-[75px] text-secondary'>Living Room Collection</h1>
-                  <button className='font-pop font-normal text-base  text-secondary px-12 hover:bg-secondary duration-500 hover:text-white py-4 rounded-full border-solid border border-secondary'>Shop Now</button>
+                  <p className='font-roboto font-normal text-sm md:text-lg text-secondary'>Get up to 60% off Today Only!</p>
+                  <h1 className='font-roboto font-bold my-4 md:my-5 w-[200px] md:w-auto text-[28px] md:text-[70px] leading-9 md:leading-[75px] text-secondary'>Living Room Collection</h1>
+                  <button className='font-pop text-sm font-normal md:text-base px-4 py-2 text-secondary md:px-12 hover:bg-secondary duration-500 hover:text-white md:py-4 rounded-full border-solid border border-secondary'>Shop Now</button>
                 </div>
                 <div className='w-6/12'>
                   <Image className="w-[500px]" src={One}/>
@@ -110,13 +109,13 @@ function Home() {
             </Container>
           </div>
 
-          <div className=' bg-[#D6F7F2] h-[580px] w-full py-[100px]'>
+          <div className=' bg-[#D6F7F2] h-[300px] md:h-[580px] w-full py-[40px] md:py-[100px] px-2 xl:px-0'>
             <Container>
               <Flex className="justify-between pt-5">
                 <div className='w-4/12'>
-                  <p className='font-roboto font-normal text-lg text-secondary'>Get up to 50% off Today Only!</p>
-                  <h1 className='font-roboto font-bold my-5 text-[70px] leading-[75px] text-secondary'>Wooden Chair Collection</h1>
-                  <button className='font-pop font-normal text-base  text-secondary px-12 hover:bg-secondary duration-500 hover:text-white py-4 rounded-full border-solid border border-secondary'>Shop Now</button>
+                  <p className='font-roboto font-normal text-sm md:text-lg text-secondary'>Get up to 60% off Today Only!</p>
+                  <h1 className='font-roboto font-bold my-4 md:my-5 w-[200px] md:w-auto text-[28px] md:text-[70px] leading-9 md:leading-[75px] text-secondary'>Living Room Collection</h1>
+                  <button className='font-pop text-sm font-normal md:text-base px-4 py-2 text-secondary md:px-12 hover:bg-secondary duration-500 hover:text-white md:py-4 rounded-full border-solid border border-secondary'>Shop Now</button>
                 </div>
                 <div className='w-6/12'>
                   <Image className="w-[500px]" src={One}/>
@@ -124,8 +123,6 @@ function Home() {
               </Flex>
             </Container>
           </div>
-          
-         
         </Slider>
    
     </section>
